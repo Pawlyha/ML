74cccd099d5392abbc0ab567518f6791f985b6e4
pawlyha <woonderluck@gmail.com>
2017-09-06 19:17:07 +0300

    Ignore datasets, human dataset added, cleared src

--- main.py
+++
@@ -1,9 +0,0 @@
-from perceptron import custom_perceptron as my
-import numpy as np
-
-p = my.CustomPerceptron(0.8, 10)
-
-data = np.genfromtxt('data/iris_dataset.csv', delimiter=',', names=True, dtype=None)
-d = np.matrix(data)
-
-[print() for xi in data]
